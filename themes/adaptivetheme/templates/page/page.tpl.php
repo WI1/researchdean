91954df7c888918461db356352225998c63786b6
dominikboehler <dominikboehler>
2011-06-14 13:50:45 +0000

    changed page-template to have tabs below title

--- themes/adaptivetheme/templates/page/page.tpl.php
+++ themes/adaptivetheme/templates/page/page.tpl.php
@@ -173,16 +173,29 @@
           <?php print $content_top; ?>
         </div> <!-- /content-top -->
         <?php endif; ?>
-        
-           <div id="main-content-header">
+		
+     	<div id="main-content-header">
+							<?php if (!empty($title)): ?>
+								<h1 id="page-title" class="title"><?php print $title; ?></h1>
+							<?php endif; ?>
+							<?php if (isset($node) && !empty($node->og_image)): ?>
+							<p class="long"><?php print $node->og_image; ?></p>
+							<?php endif; ?>
+							<?php if (!empty($tabs)): ?>
+								<div class="tabs local-tasks">
+									<?php print $tabs; ?></div>
+								<?php endif; ?>
+            
 		    <?php if ($tabs): ?>
               <div class="local-tasks"><?php print $tabs; ?></div>
             <?php endif; ?>
-            <?php if ($title): ?><h1 id="page-title"><?php if ($node->field_titel_yesno[0]['value'] == 'No') { } else {print $title; } ?></h1><?php endif; ?>
+            
             <?php if ($messages && $user->uid != 0): print $messages; endif; ?>
             <?php if ($help): print $help; endif; ?>
           </div>
-          
+    		  
+		  
+		  
           <?php if ($content_aside): ?> 
             <div id="content-aside" class="section region">
               <?php print $content_aside; ?>

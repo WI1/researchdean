246e62cb1e9b54ac9b2c53dc50f5c0cdf2a1e762
dominikboehler <dominikboehler>
2011-10-28 09:10:12 +0000

    changed og template again

--- themes/adaptivetheme/templates/og/node-forschungsfeld.tpl.php
+++ themes/adaptivetheme/templates/og/node-forschungsfeld.tpl.php
@@ -52,7 +52,9 @@
  */
 ?>
 <?php if(arg(2) === null): ?>
-<?php print $node->content['view']['#value']; $node=node_load(arg(1)); drupal_set_title($node->title); ?>
+<?php print views_embed_view('og_ghp_ron', 'default', $node->nid); $node=node_load(arg(1)); drupal_set_title($node->title); ?>
+
 <?php elseif(arg(2) == 'info'): ?>
-<?php print views_embed_view('og_members_faces', 'page_1'); ?>
+<?php //drupal_set_message('<pre>' . print_r($node, TRUE) . '</pre>'); ?>
+<?php print $content; ?>
 <?php endif; ?>
